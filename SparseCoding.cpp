fd73873b00394a11ba82ea7161a9556c6f0f2ee0
Angie Chiang <angieyayabird@gmail.com>
2016-02-15 20:05:57 -0800

    not converge

--- SparseCoding.cpp
+++ SparseCoding.cpp
@@ -5,6 +5,7 @@
 #include <tr1/unordered_map>
 
 #define EPSILON 0.02 
+#define DEBUG 1
 
 using namespace cv;
 typedef std::tr1::unordered_map<int, int> hash_map_ii;
@@ -65,7 +66,7 @@ Mat random_matrix(int rows, int cols) {
   Mat A(rows, cols, CV_64F);
   for(int r = 0; r < rows; r++) {
     for(int c = 0; c < cols; c++) {
-      A.at<double>(r, c) = rand() % (1<<8);
+      A.at<double>(r, c) = (rand() % (1<<8)) * 1. / (1<<8);
       //A.at<double>(r, c) = r+1;
     }
   }
@@ -120,11 +121,7 @@ void test_partial_differential() {
   //printf("df: %f df_ref: %f\n", df, df_ref);
 }
 
-// 2a
-// update theta_map 
-// update x
-void pick_theta_map(Mat& x, Mat A, Mat y, double r, hash_map_ii& theta_map) {
-  // x to x_map
+hash_map_if get_x_map(const hash_map_ii& theta_map, const Mat& x) {
   hash_map_if x_map;
   int xi = 0;
   for(auto& it : theta_map) {
@@ -132,6 +129,15 @@ void pick_theta_map(Mat& x, Mat A, Mat y, double r, hash_map_ii& theta_map) {
     x_map[idx] = x.at<double>(xi, 0);
     xi++;
   }
+  return x_map;
+}
+
+// 2a
+// update theta_map 
+// update x
+void pick_theta_map(Mat& x, Mat A, Mat y, double r, hash_map_ii& theta_map) {
+  // x to x_map
+  hash_map_if x_map = get_x_map(theta_map, x);
 
   // find the best partial differential of ||y - Ax||^2
   int w = A.size().width;
@@ -153,7 +159,7 @@ void pick_theta_map(Mat& x, Mat A, Mat y, double r, hash_map_ii& theta_map) {
   }
 
   x = Mat::zeros(theta_map.size(), 1, CV_64F);
-  xi = 0;
+  int xi = 0;
   for(auto& it : theta_map) {
     int idx = it.first;
     x.at<double>(xi, 0) = x_map[idx];
@@ -180,9 +186,41 @@ double QP_error(Mat x, Mat A, Mat y, double r, hash_map_ii theta_map) {
   return err_vec.dot(err_vec) + r * theta_x;
 }
 
+Mat get_theta(hash_map_ii theta_map) {
+  Mat theta(theta_map.size(), 1, CV_64F);
+  int theta_idx = 0;
+  for(auto& it : theta_map) {
+    int idx = it.first;
+    int coeff = it.second;
+    theta.at<double>(theta_idx, 0) = coeff;
+    theta_idx++;
+  }
+  return theta;
+}
+
+Mat QP_partial_differential(Mat x, Mat A, Mat y, double r, hash_map_ii theta_map) {
+  // Ax
+  Mat Ax = get_Ax(x, A, theta_map);
+  Mat y_Ax = y - Ax;
+
+  // -2 * Tr(A)(y-Ax)
+  Mat AT_y_Ax = Mat::zeros(theta_map.size(), 1, CV_64F);
+
+  int AT_y_Ax_idx = 0;
+  for(auto& it : theta_map) {
+    int idx = it.first;
+    AT_y_Ax.at<double>(AT_y_Ax_idx) = -2 * y_Ax.dot(A.col(idx));
+    AT_y_Ax_idx++;
+  }
+
+  Mat theta = get_theta(theta_map);
+  return AT_y_Ax + r*theta;
+}
+
 // 3a
 // min_x ||y - Ax||^2 + r* Tr(theta)x
 Mat QP_solution(Mat A, Mat y, double r, hash_map_ii theta_map) {
+  printf("QP_solution start ====\n");
   int x_size = theta_map.size();
 
   // get ATA
@@ -200,7 +238,7 @@ Mat QP_solution(Mat A, Mat y, double r, hash_map_ii theta_map) {
     }
     ri++;
   }
-  Mat ATA_inv = ATA.inv(); 
+  Mat ATA_inv = ATA.inv(DECOMP_SVD); 
 
   // get ATy
   Mat ATy(x_size, 1, CV_64F);
@@ -212,15 +250,24 @@ Mat QP_solution(Mat A, Mat y, double r, hash_map_ii theta_map) {
   }
 
   // get theta
-  Mat theta(x_size, 1, CV_64F);
-  int theta_idx = 0;
-  for(auto& it : theta_map) {
-    int idx = it.first;
-    int coeff = it.second;
-    theta.at<double>(theta_idx, 0) = coeff;
-    theta_idx++;
-  }
+  Mat theta = get_theta(theta_map);
   Mat x = ATA_inv * (ATy - theta * (r/2));
+#if DEBUG
+  //if(determinant(ATA) > EPSILON) {
+    Mat df = QP_partial_differential(x, A, y, r, theta_map);
+    int df_size = df.size().height;
+    for(int i = 0; i < df_size; i++) {
+      if(fabs(df.at<double>(i, 0)) > EPSILON){
+        printf("QP_solution error\n");
+      }
+    }
+    show_theta_map(theta_map);
+    show_matrix("x", x);
+    show_matrix("df", df);
+    printf("det ATA: %f\n", determinant(ATA));
+ // }
+#endif
+  printf("QP_solution end ====\n");
   return x;
 }
 
@@ -260,6 +307,7 @@ double one_norm_error(Mat x, Mat A, Mat y, double r, hash_map_ii theta_map) {
 // argmin ||y - A(x + ap)||^2 + r*Tr(theta)(x + ap)
 // update theta_map and x
 void one_norm_line_search(Mat A, Mat y, double r, hash_map_ii& theta_map, Mat& x, Mat x_new) {
+  printf("one_norm_line_search start ====\n");
   double a = 0;
   Mat p = x_new - x;
   int p_size = p.size().height;
@@ -273,7 +321,7 @@ void one_norm_line_search(Mat A, Mat y, double r, hash_map_ii& theta_map, Mat& x
     double pv = p.at<double>(pi);
     double xv = x.at<double>(pi);
     double xv_new = x_new.at<double>(pi);
-    if(sign(pv) != sign(xv_new)) {
+    if(sign(xv) != sign(xv_new)) {
       separate_points.push_back(pair_fi(-xv/pv, theta_idx));
     }
     pi++;
@@ -307,6 +355,7 @@ void one_norm_line_search(Mat A, Mat y, double r, hash_map_ii& theta_map, Mat& x
     it.second = sign(x.at<double>(x_idx));
     x_idx++;
   }
+  printf("one_norm_line_search end ====\n");
 }
 
 void test_one_norm_line_search() {
@@ -333,7 +382,15 @@ int check_nonzero_opt_condition(Mat x, Mat A, Mat y, double r, hash_map_ii theta
     int idx = it.first;
     double df = partial_differential(x, A, y, theta_map, idx);
     double vx = x.at<double>(x_idx);
-    //printf("nonzero total df: %f df: %f r*sign(vx): %f\n", fabs(df + r * sign(vx)), df, r * sign(vx));
+    printf("nonzero total df: %f df: %f r*sign(vx): %f\n", fabs(df + r * sign(vx)), df, r * sign(vx));
+    x_idx++;
+  }
+  x_idx = 0;
+  for(auto& it : theta_map) {
+    int idx = it.first;
+    double df = partial_differential(x, A, y, theta_map, idx);
+    double vx = x.at<double>(x_idx);
+    // printf("nonzero total df: %f df: %f r*sign(vx): %f\n", fabs(df + r * sign(vx)), df, r * sign(vx));
     if(fabs(df + r * sign(vx)) >= EPSILON) {
       return 0;
     }
@@ -387,7 +444,7 @@ int test_read_image(int argc, char** argv ) {
   return 0;
 }
 
-Mat feature_sign_search(Mat A, Mat y, double r) {
+hash_map_if feature_sign_search(Mat A, Mat y, double r) {
   Mat x; 
   hash_map_ii theta_map;
   do {
@@ -395,24 +452,38 @@ Mat feature_sign_search(Mat A, Mat y, double r) {
     do {
       Mat x_new = QP_solution(A, y, r, theta_map);
       one_norm_line_search(A, y, r, theta_map, x, x_new);
+      show_theta_map(theta_map);
+      show_matrix("x", x);
+      double err = one_norm_error(x, A, y, r, theta_map);
+      double QP_err = QP_error(x, A, y, r, theta_map);
+      printf("err: %f QP_err: %f\n", err, QP_err);
     } while(!check_nonzero_opt_condition(x, A, y, r, theta_map));
   } while(!check_zero_opt_condition(x, A, y, r, theta_map));
-  show_theta_map(theta_map);
-  show_matrix("x", x);
-  return x; 
+  hash_map_if x_map = get_x_map(theta_map, x);
+  return x_map; 
 }
 
 void test_feature_sign_search() {
   double r = 0.1;
-  Mat A = random_matrix(3, 6);
-  Mat y = random_matrix(3, 1);
-  Mat x = feature_sign_search(A, y, r);
+  Mat A = random_matrix(8, 15);
+  Mat y = random_matrix(8, 1);
+  hash_map_if x_map = feature_sign_search(A, y, r);
+}
+
+void test_inverse() {
+  Mat A = random_matrix(3, 3);
+  for(int i = 0; i < 3; i++) {
+    A.at<double>(0, i) = 0;
+  }
+  show_matrix("A*inv(A)*A", A*A.inv(DECOMP_SVD)*A);
+  show_matrix("A", A);
 }
 
 int main() {
   //test_QP_solution();
   //test_one_norm_line_search();
   //test_partial_differential();
+  //test_inverse();
   test_feature_sign_search();
 }
 
